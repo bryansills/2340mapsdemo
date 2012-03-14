ba7dce2ae9cf76fd3f53bad5cdc2ac625de447a4
Bryan Sills <bryan.a.sills@gmail.com>
2012-03-14 02:02:07 -0400

    initial commit

---
+++ src/org/gitmad/maps/mapsdemoActivity.java
@@ -0,0 +1,13 @@
+package org.gitmad.maps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mapsdemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
