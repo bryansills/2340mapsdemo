335ac6ccc7ab24fe7905fcba13ab1fd3b8647af7
Bryan Sills <bryan.a.sills@gmail.com>
2012-03-26 11:01:01 -0400

    built demo. be aware that the appi key is not in the commit. you need to add your own.

--- src/org/gitmad/maps/mapsdemoActivity.java
+++ src/org/gitmad/maps/mapsdemoActivity.java
@@ -1,13 +1,28 @@
 package org.gitmad.maps;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class mapsdemoActivity extends Activity {
+public class mapsdemoActivity extends MapActivity {
     /** Called when the activity is first created. */
+	
+	LinearLayout linearLayout;
+	MapView mapView;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
